c54a5a54622fd879142c3a580c8a382090f0843b
Satya-roy <samantroy04@gmail.com>
2021-05-19 23:46:13 +0530

    add layout header and footer to views folder

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
 
+app.use(expressLayouts);
 //use express router, need to tell express using middleware
 app.use('/',require('./routes/index'));
+
+
 //set view engine
 app.set('view engine','ejs');
 app.set('views','./views');
