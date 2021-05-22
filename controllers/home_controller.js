989af1908936666b5260da7779dd80fc4b9de9dd
Satya-roy <samantroy04@gmail.com>
2021-05-22 20:30:46 +0530

    show all the post populate the post with users data

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,19 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req,res){
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title: "Home",
+    //         posts: posts
+    //     });
+    // });
     
-    return res.render('home',{
-        title: "Home"
+    // populating the user of each post
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title: "Home",
+            posts: posts
+        });
     });
+
 };
