f1b4df0731c24bdeabe21d8d06e22bf04d6156ec
Satya-roy <samantroy04@gmail.com>
2021-05-23 02:36:55 +0530

    populate post with comments & name of the person who wrote the comment

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -9,7 +9,15 @@ module.exports.home = function(req,res){
     // });
     
     // populating the user of each post
-    Post.find({}).populate('user').exec(function(err,posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate :{
+            path: 'user'
+        }
+    })
+    .exec(function(err,posts){
         return res.render('home',{
             title: "Home",
             posts: posts
