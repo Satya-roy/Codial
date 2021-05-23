ab3dbece94f0fa7016398e0887e2f85449a7490f
Satya-roy <samantroy04@gmail.com>
2021-05-23 22:12:29 +0530

    clean call back functions in controllers using asyun await

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,40 +1,51 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-module.exports.create = function(req,res){
+module.exports.create = async function(req,res){
     //if post exist then create the comment
-    Post.findById(req.body.post, function(err,post){
-        console.log(`post controller`);
+    try{
+        let post = await Post.findById(req.body.post);
+
         if(post){
-            Comment.create({
+            let comment = await Comment.create({
                 content: req.body.content,
                 post:req.body.post,
                 user:req.user._id
-            }, function(err,comment){
-                //handle error
-                post.comments.push(comment);
-                post.save();
-
-                res.redirect('/');
             });
+
+            post.comments.push(comment);
+            post.save();
+
+            res.redirect('/');
         }
-    });
+    }
+    catch(err){
+        console.log('Error',err);
+        return;
+    }
+    
 };
 
-module.exports.destroy = function(req,res){
-    Comment.findById(req.params.id,function(err,comment){
+module.exports.destroy = async function(req,res){
+    try{
+        let comment = await Comment.findById(req.params.id);
+
         if(comment && comment.user==req.user.id){ //.id converts it into string
 
-            let postId = comment.post;
+                let postId = comment.post;
 
-            comment.remove();
-            // pull helps to pull the comment from the comment array and remove it
-            // syntax used in mongodb so mongoose can use aswell
+                comment.remove();
+                // pull helps to pull the comment from the comment array and remove it
+                // syntax used in mongodb so mongoose can use aswell
             Post.findByIdAndUpdate(postId,{ $pull :{comments: req.params.id}},function(err,post){
-                return redirect('back');
+                return res.redirect('back');
             });
         }else{
             return res.redirect('back');
         }
-    });
+    }
+    catch(err){
+        console.log('Error',err);
+        return;
+    }
 }
--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,7 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function(req,res){
+module.exports.home = async function(req,res){
     // Post.find({},function(err,posts){
     //     return res.render('home',{
     //         title: "Home",
@@ -10,26 +10,36 @@ module.exports.home = function(req,res){
     // });
     
     // populating the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate :{
-            path: 'user'
-        }
-    })
-    .exec(function(err,posts){
 
-        User.find({},function(err,users){
-            return res.render('home',{
-                title: "Home",
-                posts: posts,
-                all_users:users
-            });
+    try{
 
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate :{
+                path: 'user'
+            }
+        })
+        
+    
+        let users = await User.find({});
+    
+        return res.render('home',{
+            title: "Home",
+            posts: posts,
+            all_users:users
         });
 
+    }
+    catch(err){
+        console.log(`Error`, err);
+        return;
+    }
+
+    
+
         
-    });
+    
 
 };
